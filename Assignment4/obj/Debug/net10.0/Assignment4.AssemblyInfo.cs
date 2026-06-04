dc9c44732bcb964d6be87f9341b22d3b6dfc1ddf
Maaz Qaisar <247238@students.au.edu.pk>
2026-06-04 19:22:22 +0500

    Add Application 1 component with registration form and data validation

--- Assignment4/obj/Debug/net10.0/Assignment4.AssemblyInfo.cs
+++ Assignment4/obj/Debug/net10.0/Assignment4.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assignment4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+633ac5ebfd6b0b2decf68d83a2effaa4fe5a2a57")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assignment4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assignment4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
